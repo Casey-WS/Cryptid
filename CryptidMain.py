87ef83f31d336c487b456fddfde9d44da52dc145
Casey <casey-ws@github.com>
2021-01-24 14:09:11 -0800

    Tidying up

--- CryptidMain.py
+++ CryptidMain.py
@@ -6,7 +6,9 @@ import Cryptid as g
 import random
 import time
 
-class CryptidA:
+# A basic cryptid which purely returns a random
+# move
+class RandomCryptid:
   def __init__(self):
     self.a = 1
   def getMove(self, metadata):
@@ -14,7 +16,9 @@ class CryptidA:
   def __str__(self):
     return 'A'
 
-class CryptidB:
+# A cryptid which will attack enemies in front,
+# but behaves randomly otherwise
+class Triffid:
   def __init__(self):
     self.a = 1
   def getMove(self, metadata):
@@ -24,6 +28,8 @@ class CryptidB:
   def __str__(self):
     return 'B'
 
+# A cryptid which will turn to attack enemies nearby,
+# but moves randomly otherwise
 class Afanc:
   def __init__(self):
     self.a = 1
@@ -36,14 +42,17 @@ class Afanc:
       return 1
     if metadata.getBack() == 3:
       return 2
-    return 2
+    return random.randint(0, 3)
   def __str__(self):
     return 'C'
 
+# Set up a simple game with 30 of each
 cg = g.Game(width = 40, height = 30)
-cg.add(30, CryptidA)
-cg.add(30, CryptidB)
-cg.add(50, Afanc)
+cg.add(30, RandomCryptid)
+cg.add(30, Triffid)
+cg.add(30, Afanc)
+
+# Run the simulation and print the board after each update
 print(cg)
 while True:
   cg.update()
